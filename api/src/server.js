b4605fbc88adb54581fdc8c8586482ea33fd32ec
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-01-13 13:39:27 -0300

    style: aplicando estilo

--- api/src/server.js
+++ api/src/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const dbPool = require("./db/connection");
-const schema = require("./db/schema")
-const seeds = require("./db/seeds")
+const schema = require("./db/schema");
+const seeds = require("./db/seeds");
 
 const app = express();
 const PORT = 3003;
 
 app.use(express.json());
 
-schema()
-seeds()
+schema();
+seeds();
 
 app.get("/", async (request, response) => {
   let connection;
@@ -18,20 +18,23 @@ app.get("/", async (request, response) => {
     connection = await dbPool.promise().getConnection();
 
     const users = await connection.query("SELECT * FROM people");
- 
+
     const htmlContent = `
       <html>
         <body>
           <h1>Full Cycle Rocks!</h1>
-          <ul>${users[0].map(user => `
-            <li>${user.nome}</li>`).join('')}
+          <ul>${users[0]
+            .map(
+              (user) => `
+            <li>${user.nome}</li>`
+            )
+            .join("")}
           </ul>
         </body>
       </html>`;
 
-    response.setHeader('Content-Type', 'text/html');
+    response.setHeader("Content-Type", "text/html");
     response.send(htmlContent);
-   
   } catch (error) {
     console.error("Error fetching data:", error);
     response.status(500).send("Internal Server Error");
